55364db2feee5a3b51d1710f4c8fc543e7c1316c
hatsuyama <hatsuyama>
2025-03-08 16:39:43 +0900

    add

--- SchedulerTime2/Program.cs
+++ SchedulerTime2/Program.cs
@@ -3,11 +3,20 @@ using Microsoft.Extensions.Hosting;
 using System.Data;
 using Microsoft.Data.SqlClient;
 using ClassLibrary;
+using Microsoft.Extensions.Configuration;
 
 var host = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+            //config.AddEnvironmentVariables();
+        })
     .ConfigureServices((context, services) =>
     {
-        services.AddSingleton<IDbConnection>(sp => new SqlConnection("YourConnectionString"));
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+
+        services.AddSingleton<IDbConnection>(sp => new SqlConnection(connectionString));
         services.AddSingleton<IHolidaysProvider, DummyHolidaysProvider>(); // 祝日プロバイダ
         services.AddSingleton<Scheduler>(); // `Scheduler` に `IHolidaysProvider` を注入
         services.AddHostedService<HolidaysService>(); // `IHostedService` として登録
