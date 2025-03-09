d7b8a700d0bd1584326d811b84c12a62297d998e
hatsuyama <hatsuyama>
2025-03-09 10:38:21 +0900

    add

--- SchedulerTime2/Program.cs
+++ SchedulerTime2/Program.cs
@@ -17,6 +17,24 @@ var host = Host.CreateDefaultBuilder(args)
         var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
 
         services.AddSingleton<IDbConnection>(sp => new SqlConnection(connectionString));
+
+        //services.AddKeyedSingleton<IDbConnection>("Default", (sp, _) =>
+        //{
+        //    var connectionString = sp.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection");
+        //    return new SqlConnection(connectionString);
+        //});
+
+        //services.AddKeyedSingleton<IDbConnection>("Secondary", (sp, _) =>
+        //{
+        //    var connectionString = sp.GetRequiredService<IConfiguration>().GetConnectionString("SecondaryConnection");
+        //    return new SqlConnection(connectionString);
+        //});
+        //using var scope = app.Services.CreateScope();
+        //var serviceProvider = scope.ServiceProvider;
+
+        //var defaultDb = serviceProvider.GetKeyedService<IDbConnection>("Default");
+        //var secondaryDb = serviceProvider.GetKeyedService<IDbConnection>("Secondary");
+
         services.AddSingleton<IHolidaysProvider, DummyHolidaysProvider>(); // 祝日プロバイダ
         services.AddSingleton<Scheduler>(); // `Scheduler` に `IHolidaysProvider` を注入
         services.AddHostedService<HolidaysService>(); // `IHostedService` として登録
